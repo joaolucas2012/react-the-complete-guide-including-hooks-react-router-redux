be510080460db3922815a92552808a0e3586f01e
unknown <ribeirojoaolucas68@gmail.com>
2022-01-31 23:36:13 -0300

    feat: review of classes and methods updated to ES6

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 // REVIEWING CLASSES
+/*
 class Human {
   constructor() {
     this.gender = "male";
@@ -19,6 +20,30 @@ class Person extends Human {
   printMyName() {
     console.log(this.name);
   }
+}
+const person = new Person();
+person.printMyName();
+person.printGender();
+*/
+
+///////////////////////////////////////////////////
+
+// REVIEW OF CLASSES AND METHODS
+class Human {
+  gender = "male";
+
+  printGender = () => {
+    console.log(this.gender);
+  };
+}
+
+class Person extends Human {
+  name = "Max";
+  gender = "Female";
+
+  printMyName = () => {
+    console.log(this.name);
+  };
 }
 const person = new Person();
 person.printMyName();
